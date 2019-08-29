ac6f4ae87cc435684f6502316cf87c186072868a
810485328@qq.com <810485328@qq.com>
2019-08-29 15:49:52 +0800

    config change

--- playbooks/roles/keepalived/templates/check_ng.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-if [ $(ps -C nginx --no-header | wc -l) -eq 0 ]; then
-    systemctl start nginx
-    sleep 1
-fi
-
-if [ $(ps -C nginx --no-header | wc -l) -eq 0 ]; then
-    systemctl stop keepalived
-fi
--- playbooks/roles/keepalived/templates/mailnotify.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-mail="billie@gmail.com"
-echo "$1" |/bin/mail -s "hostname keeplived status change" $mail
